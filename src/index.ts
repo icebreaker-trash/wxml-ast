b98467abcbf5c4bfd42cd40a5ddbcf39f2e935f8
SonOfMagic <qq1324318532@gmail.com>
2022-12-02 15:36:05 +0800

    chore: code parse5 commit

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,9 @@
-import { Parser } from 'htmlparser2'
-import { DomHandler } from 'domhandler'
-import rawRender from 'wxml-dom-serializer'
-import defu from 'defu'
-import type {
-  UserDefinedOptions,
-  DomSerializerOptions,
-  ChildNode,
-  AnyNode
-} from './types'
+import parse5 from 'parse5'
+import htmlparser2 from 'parse5-htmlparser2-tree-adapter'
 
-export function parse (
-  chunk: string,
-  options: UserDefinedOptions = {}
-): Promise<ChildNode[]> {
-  const defaultOptions: UserDefinedOptions = {}
-  const opt = defu(options, defaultOptions)
-  return new Promise((resolve, reject) => {
-    const handler = new DomHandler(
-      (error, dom) => {
-        if (error) {
-          reject(error)
-        } else {
-          resolve(dom)
-        }
-      },
-      opt.domHandlerOptions,
-      opt.elementCB
-    )
-
-    const parser = new Parser(handler, opt.parserOptions)
-    parser.write(chunk)
-    parser.end()
+export function parse (content: string) {
+  const tree = parse5.parse(content, {
+    treeAdapter: htmlparser2.adapter
   })
-}
-
-export function render (
-  node: AnyNode | ArrayLike<AnyNode>,
-  options: DomSerializerOptions = {}
-) {
-  const defaultOptions: DomSerializerOptions = {
-    // encodeEntities: false,
-    selfClosingTags: false,
-    decodeEntities: false
-    // encodeEntities: false
-  }
-  const opt = defu(options, defaultOptions)
-  return rawRender(node, opt)
+  return tree
 }
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import type { DomSerializerOptions } from 'wxml-dom-serializer'
-import type { ChildNode, DomHandlerOptions, Element, AnyNode } from 'domhandler'
-import type { ParserOptions } from 'htmlparser2'
-export interface UserDefinedOptions {
-  parserOptions?: ParserOptions
-  domHandlerOptions?: DomHandlerOptions
-  elementCB?: (element: Element) => void
-}
-export { DomSerializerOptions, ChildNode, AnyNode }
